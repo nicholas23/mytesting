c5f774c6530a4da4bec8fe9b383d0897d9e07225
Chaoshen Hsu <jnick23@gmail.com>
2021-06-05 09:17:23 +0800

    init project

---
+++ src/main/java/com/example/mytesting/MytestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytesting/MytestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
